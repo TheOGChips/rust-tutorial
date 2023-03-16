fbb731e123795c8cae40f789e5dbcbf097376ff4
TheOGChips <swindell.christian.g@gmail.com>
2023-03-16 01:31:01 -0500

    Disabled printing out randomly generated number at beginning

--- ch2-programming-a-guessing-game/guessing_game/src/main.rs
+++ ch2-programming-a-guessing-game/guessing_game/src/main.rs
@@ -1,12 +1,12 @@
-use std::io;
 use rand::Rng;
 use std::cmp::Ordering;
+use std::io;
 
 fn main() {
     println!("Guess the number!");
     let secret_number = rand::thread_rng().gen_range(1..=100);
-    println!("The secret number is: {secret_number}");
-    
+    //println!("The secret number is: {secret_number}");
+
     loop {
         println!("Please input your guess.");
         let mut guess = String::new();
@@ -17,7 +17,7 @@ fn main() {
             Ok(num) => num,
             Err(_) => continue,
         };
-            
+
         println!("You guessed: {guess}");
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
