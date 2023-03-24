df87605736db62f09f1396b77180d6b237f0a654
TheOGChips <swindell.christian.g@gmail.com>
2023-03-24 00:50:20 -0500

    Finished "Defining Modules to Control Scope and Privacy" section

---
+++ ch7-managing-growing-projects-with-packages-crates-and-modules/backyard/target/debug/deps/backyard-8b2db7143e38a1c7.d
@@ -0,0 +1,7 @@
+/home/chris/Documents/projects/rust-tutorial/ch7-managing-growing-projects-with-packages-crates-and-modules/backyard/target/debug/deps/backyard-8b2db7143e38a1c7: src/main.rs src/garden.rs src/garden/vegetables.rs
+
+/home/chris/Documents/projects/rust-tutorial/ch7-managing-growing-projects-with-packages-crates-and-modules/backyard/target/debug/deps/backyard-8b2db7143e38a1c7.d: src/main.rs src/garden.rs src/garden/vegetables.rs
+
+src/main.rs:
+src/garden.rs:
+src/garden/vegetables.rs:
