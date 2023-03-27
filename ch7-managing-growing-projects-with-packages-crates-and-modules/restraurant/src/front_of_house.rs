3fc9214ea7443f14ca256c6763cc1f8d4fd02a5a
TheOGChips <swindell.christian.g@gmail.com>
2023-03-27 01:21:29 -0500

    Finished "Separating Modules Into Different Files" section

---
+++ ch7-managing-growing-projects-with-packages-crates-and-modules/restraurant/src/front_of_house.rs
@@ -0,0 +1,8 @@
+pub mod hosting;
+// NOTE: Could also put this code into front_of_house/mod.rs
+
+mod serving {
+    fn take_order () {}
+    fn serve_order () {}
+    fn take_payment () {}
+}
--- ch7-managing-growing-projects-with-packages-crates-and-modules/restraurant/target/debug/deps/restraurant-67b11632294f3e07.d
+++ ch7-managing-growing-projects-with-packages-crates-and-modules/restraurant/target/debug/deps/restraurant-67b11632294f3e07.d
@@ -1,5 +1,7 @@
-/home/chris/Documents/projects/rust-tutorial/ch7-managing-growing-projects-with-packages-crates-and-modules/restraurant/target/debug/deps/restraurant-67b11632294f3e07.rmeta: src/lib.rs
+/home/chris/Documents/projects/rust-tutorial/ch7-managing-growing-projects-with-packages-crates-and-modules/restraurant/target/debug/deps/restraurant-67b11632294f3e07.rmeta: src/lib.rs src/front_of_house.rs src/front_of_house/hosting.rs
 
-/home/chris/Documents/projects/rust-tutorial/ch7-managing-growing-projects-with-packages-crates-and-modules/restraurant/target/debug/deps/restraurant-67b11632294f3e07.d: src/lib.rs
+/home/chris/Documents/projects/rust-tutorial/ch7-managing-growing-projects-with-packages-crates-and-modules/restraurant/target/debug/deps/restraurant-67b11632294f3e07.d: src/lib.rs src/front_of_house.rs src/front_of_house/hosting.rs
 
 src/lib.rs:
+src/front_of_house.rs:
+src/front_of_house/hosting.rs:
