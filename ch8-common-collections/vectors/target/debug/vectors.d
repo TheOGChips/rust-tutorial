87f2525865a903365017300fa3754af0a1076f88
TheOGChips <swindell.christian.g@gmail.com>
2023-03-28 03:47:52 -0500

    Finished "Storing Lists of Values with Vectors" section

---
+++ ch8-common-collections/vectors/target/debug/vectors.d
@@ -0,0 +1,1 @@
+/home/chris/Documents/projects/rust-tutorial/ch8-common-collections/vectors/target/debug/vectors: /home/chris/Documents/projects/rust-tutorial/ch8-common-collections/vectors/src/main.rs
