78f8e16d8490de1959cc58a21b34d6f304d9506b
TheOGChips <swindell.christian.g@gmail.com>
2023-04-03 00:03:30 -0500

    Finished "To panic! or Not to panic!" section

---
+++ ch9-error-handling/struct_error_checking/target/debug/deps/struct_error_checking-ff47b72fcb25fc61.d
@@ -0,0 +1,5 @@
+/home/chris/Documents/projects/rust-tutorial/ch9-error-handling/struct_error_checking/target/debug/deps/struct_error_checking-ff47b72fcb25fc61.rmeta: src/main.rs
+
+/home/chris/Documents/projects/rust-tutorial/ch9-error-handling/struct_error_checking/target/debug/deps/struct_error_checking-ff47b72fcb25fc61.d: src/main.rs
+
+src/main.rs:
