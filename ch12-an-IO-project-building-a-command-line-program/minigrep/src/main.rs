4835199bbc773cac0579b67d2b2811457ec966c4
TheOGChips <swindell.christian.g@gmail.com>
2023-04-09 19:20:11 -0500

    Finished "Reading a File" section

--- ch12-an-IO-project-building-a-command-line-program/minigrep/src/main.rs
+++ ch12-an-IO-project-building-a-command-line-program/minigrep/src/main.rs
@@ -1,10 +1,15 @@
 use std::env;
+use std::fs;
 
-fn main() {
+fn main () {
     let args: Vec<String> = env::args().collect();
     let query = &args[1];
-    let file_path = &args[2];
+    let filepath = &args[2];
 
     println!("Searching for {}", query);
-    println!("In file {}", file_path);
+    println!("In file {}", filepath);
+
+    let contents = fs::read_to_string(filepath)
+        .expect("Should have been able to read the file");
+    println!("With text:\n{contents}");
 }
