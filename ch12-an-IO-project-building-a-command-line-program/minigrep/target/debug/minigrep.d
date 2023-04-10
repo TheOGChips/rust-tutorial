6b6470a7776273f878845e7b91b241ee1d18a925
TheOGChips <swindell.christian.g@gmail.com>
2023-04-09 19:12:19 -0500

    Finished "Accepting Command Line Arguments" section

---
+++ ch12-an-IO-project-building-a-command-line-program/minigrep/target/debug/minigrep.d
@@ -0,0 +1,1 @@
+/Users/chris/Documents/rust-tutorial/ch12-an-IO-project-building-a-command-line-program/minigrep/target/debug/minigrep: /Users/chris/Documents/rust-tutorial/ch12-an-IO-project-building-a-command-line-program/minigrep/src/main.rs
