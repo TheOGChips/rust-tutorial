f83b240f251c3b99ca861680b41a580660765749
TheOGChips <swindell.christian.g@gmail.com>
2023-04-15 00:27:33 -0500

    Finished "Pattern Syntax" section

---
+++ ch18-patterns-and-matching/pattern_syntax/target/debug/deps/pattern_syntax-db4cf93547f526b1.d
@@ -0,0 +1,5 @@
+/Users/chris/Documents/rust-tutorial/ch18-patterns-and-matching/pattern_syntax/target/debug/deps/pattern_syntax-db4cf93547f526b1: src/main.rs
+
+/Users/chris/Documents/rust-tutorial/ch18-patterns-and-matching/pattern_syntax/target/debug/deps/pattern_syntax-db4cf93547f526b1.d: src/main.rs
+
+src/main.rs:
---
+++ ch18-patterns-and-matching/pattern_syntax/target/debug/pattern_syntax.d
@@ -0,0 +1,1 @@
+/Users/chris/Documents/rust-tutorial/ch18-patterns-and-matching/pattern_syntax/target/debug/pattern_syntax: /Users/chris/Documents/rust-tutorial/ch18-patterns-and-matching/pattern_syntax/src/main.rs
