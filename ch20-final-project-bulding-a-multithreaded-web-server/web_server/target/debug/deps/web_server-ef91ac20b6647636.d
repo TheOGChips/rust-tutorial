e926bbff7c6d48ef2a6566ac3f2ad6b55e975fde
TheOGChips <swindell.christian.g@gmail.com>
2023-04-16 02:34:44 -0500

    Finished "Building a Single-Threaded Web Server" section

---
+++ ch20-final-project-bulding-a-multithreaded-web-server/web_server/target/debug/deps/web_server-ef91ac20b6647636.d
@@ -0,0 +1,5 @@
+/Users/chris/Documents/rust-tutorial/ch20-final-project-bulding-a-multithreaded-web-server/web_server/target/debug/deps/web_server-ef91ac20b6647636: src/main.rs
+
+/Users/chris/Documents/rust-tutorial/ch20-final-project-bulding-a-multithreaded-web-server/web_server/target/debug/deps/web_server-ef91ac20b6647636.d: src/main.rs
+
+src/main.rs:
---
+++ ch20-final-project-bulding-a-multithreaded-web-server/web_server/target/debug/deps/web_server-f97f7caa40504c00.d
@@ -0,0 +1,5 @@
+/Users/chris/Documents/rust-tutorial/ch20-final-project-bulding-a-multithreaded-web-server/web_server/target/debug/deps/web_server-f97f7caa40504c00.rmeta: src/main.rs
+
+/Users/chris/Documents/rust-tutorial/ch20-final-project-bulding-a-multithreaded-web-server/web_server/target/debug/deps/web_server-f97f7caa40504c00.d: src/main.rs
+
+src/main.rs:
