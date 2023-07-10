3632366dc9971beb837832cfa901d56f81c7e050
TheOGChips <swindell.christian.g@gmail.com>
2023-07-09 22:04:36 -0500

    Finished "Starting with Cursive Part 2"

---
+++ cursive-tutorial/starting-with-cursive-p2/src/main.rs
@@ -0,0 +1,34 @@
+use cursive::{
+    Cursive,
+    CursiveRunnable,
+    views::{
+        Dialog,
+    },
+};
+
+fn main () {
+    let mut siv: CursiveRunnable = cursive::default();
+    siv.add_layer(Dialog::text("This is a survey!\nPress <Next> when you're ready.")
+        .title("Important survey")
+        .button("Next", show_next)
+    );
+    siv.run();
+}
+
+fn show_next (s: &mut Cursive) {
+    s.pop_layer();
+    s.add_layer(Dialog::text("Did you do the thing?")
+        .title("Question 1")
+        .button("Yes!", |s| show_answer(s, "I knew it! Well done!"))
+        .button("No!", |s| show_answer(s, "I knew you couldn't be trusted!"))
+        .button("Uh?", |s| s.add_layer(Dialog::info("Try again!")))
+    );
+}
+
+fn show_answer (s: &mut Cursive, msg: &str) {
+    s.pop_layer();
+    s.add_layer(Dialog::text(msg)
+        .title("Results")
+        .button("Finish", |s| s.quit())
+    )
+}
