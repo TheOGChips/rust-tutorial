bf83faf00d0fefe0a3784ca0481bf605322a64ef
TheOGChips <swindell.christian.g@gmail.com>
2024-01-16 19:53:11 -0600

    Creates object file in target/lib now

--- nasm-integration/build.rs
+++ nasm-integration/build.rs
@@ -5,10 +5,10 @@
 fn main() {
     nasm_rs::Build::new()
         .file("src/add2.asm")
-        .out_dir("target")
+        .out_dir("target/lib")
         .compile_objects()
         .unwrap();
     cc::Build::new()
-        .object("target/add2.o")
+        .object("target/lib/add2.o")
         .compile("add2");
 }
